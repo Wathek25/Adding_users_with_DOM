3641ef6cd92d9c146ddcc356f8ba2a8031e96ac9
Wathek25 <86679259+Wathek25@users.noreply.github.com>
2021-07-28 16:01:18 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,5 @@
-// Changing into html with javascipt
-
-// document.querySelector(".Name").innerHTML = "User name";
-
 const Name = document.querySelector('.Name')
 Name.textContent = 'User name:'
-// Name.style.color = 'red'
 
 // ADDING USERS
 
@@ -19,17 +14,15 @@ form.addEventListener ('submit', onSubmit)
 function onSubmit (e) {
     e.preventDefault();
     if (name.value === '' || email.value === '') {
-        msg.classList.add('error') // adding css class error to get the red backround
+        msg.classList.add('error') // adding css class error
         msg.innerHTML = "Please insert a valid name and email!"
         setTimeout(function(){ msg.remove() }, 2500) //removing the error message after 2.5 seconds
     }
     else {
         const li = document.createElement('li') // creating lists
-        //appendChild to add the name and email inside the li
         li.classList.add('border')
         li.appendChild(document.createTextNode(`Name : ${name.value} | Email : ${email.value} `))
         users.appendChild(li) //adding li inside the ul
-        
         //Clearing fileds after submitting
         name.value = ''
         email.value= ''
